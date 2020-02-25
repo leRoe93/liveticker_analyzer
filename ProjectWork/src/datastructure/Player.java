bba91470aed90ac0fcae850d73f2f0d49399f04f
Leon Römer <roemerl@uni-hildesheim.de>
2020-02-25 14:33:06 +0100

    [ORG] Initialized a class that represents information about a single player.

---
+++ ProjectWork/src/datastructure/Player.java
@@ -0,0 +1,19 @@
+package datastructure;
+
+public class Player {
+	
+	private enum Position {
+	    GOALKEEPER,
+	    DEFENSE,
+	    MIDFIELD,
+	    STORM
+	  }
+	
+	private String firstName;
+	private String lastName;
+	
+	private String club;
+	private Position position;
+	
+
+}
