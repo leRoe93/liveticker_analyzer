4e78a9a9f1d8c9ce4e49a4ae3e6cebdcf0cb34fd
Leon Römer <roemerl@uni-hildesheim.de>
2020-03-14 18:43:04 +0100

    [IMPL] Added position based weighted function and extended retrieval.

--- ProjectWork/src/servlets/QueryServlet.java
+++ ProjectWork/src/servlets/QueryServlet.java
@@ -19,10 +19,14 @@ import de.dfki.mycbr.core.casebase.Instance;
 import de.dfki.mycbr.core.model.AttributeDesc;
 import de.dfki.mycbr.core.model.Concept;
 import de.dfki.mycbr.core.model.IntegerDesc;
+import de.dfki.mycbr.core.model.StringDesc;
 import de.dfki.mycbr.core.model.SymbolDesc;
 import de.dfki.mycbr.core.retrieval.Retrieval;
 import de.dfki.mycbr.core.retrieval.Retrieval.RetrievalMethod;
+import de.dfki.mycbr.core.similarity.AdvancedIntegerFct;
+import de.dfki.mycbr.core.similarity.AmalgamationFct;
 import de.dfki.mycbr.core.similarity.Similarity;
+import de.dfki.mycbr.core.similarity.config.AmalgamationConfig;
 import de.dfki.mycbr.util.Pair;
 
 
@@ -59,10 +63,17 @@ public class QueryServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 		
-		String age = request.getParameter("age");
+		int age = Integer.parseInt(request.getParameter("age"));
 		String gender = request.getParameter("gender");
 		String league = request.getParameter("league");
 		String preferred_position = request.getParameter("preferred_position");
+		int offensive = Integer.parseInt(request.getParameter("offensive"));
+		int defensive = Integer.parseInt(request.getParameter("defensive"));
+		int fairplay = Integer.parseInt(request.getParameter("fairplay"));
+		int passing = Integer.parseInt(request.getParameter("passing"));
+		int vitality = Integer.parseInt(request.getParameter("vitality"));
+		int duels = Integer.parseInt(request.getParameter("duels"));
+
 		
 		
 		// MyCBR setup
@@ -80,23 +91,49 @@ public class QueryServlet extends HttpServlet {
 			}
 			
 			
+			AmalgamationFct customFct = generateWeightedFct(myConcept, preferred_position);
+			myConcept.setActiveAmalgamFct(customFct);
+			
+			
+			
+			// Getting all the attribute descs from the concept to add attributes to the case instance
+
+			IntegerDesc ageDesc = (IntegerDesc) myConcept.getAttributeDesc("age");		
+			IntegerDesc offensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("offensive");
+			IntegerDesc defensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("defensive");
+			IntegerDesc fairplayDesc = (IntegerDesc) myConcept.getAttributeDesc("fairplay");
+			IntegerDesc passingDesc = (IntegerDesc) myConcept.getAttributeDesc("passing");
+			IntegerDesc vitalityDesc = (IntegerDesc) myConcept.getAttributeDesc("vitality");
+			IntegerDesc duelsDesc = (IntegerDesc) myConcept.getAttributeDesc("duels");
+			
+			
+			SymbolDesc genderDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("gender");
+			SymbolDesc leagueDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("league");
+			SymbolDesc positionDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("preferred_position");
 			
 			Retrieval ret = new Retrieval(myConcept, cb);
 			ret.setRetrievalMethod(RetrievalMethod.RETRIEVE_SORTED);
 			Instance query = ret.getQueryInstance();
-			
 			// Adding attributes to the query
-			IntegerDesc ageDesc = (IntegerDesc) myConcept.getAllAttributeDescs().get("age");
 			query.addAttribute(ageDesc,ageDesc.getAttribute(age));
 			
-			SymbolDesc genderDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("gender");
 			query.addAttribute(genderDesc,genderDesc.getAttribute(gender));
 			
-			SymbolDesc leagueDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("league");
 			query.addAttribute(leagueDesc,leagueDesc.getAttribute(league));
 			
-			SymbolDesc positionDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("preferred_position");
 			query.addAttribute(positionDesc,positionDesc.getAttribute(preferred_position));
+			
+			query.addAttribute(offensiveDesc,ageDesc.getAttribute(offensive));
+			
+			query.addAttribute(defensiveDesc,genderDesc.getAttribute(defensive));
+			
+			query.addAttribute(fairplayDesc,leagueDesc.getAttribute(fairplay));
+			
+			query.addAttribute(passingDesc,ageDesc.getAttribute(passing));
+			
+			query.addAttribute(vitalityDesc,genderDesc.getAttribute(vitality));
+			
+			query.addAttribute(duelsDesc,leagueDesc.getAttribute(duels));
 			
 			ret.start();
 			
@@ -106,8 +143,8 @@ public class QueryServlet extends HttpServlet {
 			// Dynamic table creation for results.jsp
 			StringBuilder tableContent = new StringBuilder();
 			
-			tableContent.append("<table class='table' id='resultTable'>");
-			tableContent.append("<tr>"
+			tableContent.append("<table class='table text-center' id='resultTable'>");
+			tableContent.append("<thead class='text-center'><tr>"
 					+ "<th>Profillink</th>"
 					+ "<th>Vorname</th>"
 					+ "<th>Nachname</th>"
@@ -116,7 +153,7 @@ public class QueryServlet extends HttpServlet {
 					+ "<th>Liga</th>"
 					+ "<th>Bevorzugte Position</th>"
 					+ "<th>Ähnlichkeit</th>"
-					+ "</tr>");
+					+ "</tr><thead>");
 			
 			String[] resultAttributes = { "first_name", "last_name", "age", "gender", "league", "preferred_position" };
 			
@@ -142,6 +179,11 @@ public class QueryServlet extends HttpServlet {
 			
 			tableContent.append("</table>");
 			
+			
+			for (AttributeDesc attDesc : query.getAttributes().keySet()) {
+				request.setAttribute(attDesc.getName(), customFct.getWeight(attDesc));
+			}
+			
 			request.setAttribute("results", tableContent);
 			
 		} catch (Exception e) {
@@ -152,6 +194,62 @@ public class QueryServlet extends HttpServlet {
 		
 		
 		request.getRequestDispatcher("/results.jsp").forward(request, response);
+	}
+	
+	private AmalgamationFct generateWeightedFct(Concept myConcept, String position) {
+		AmalgamationFct function = new AmalgamationFct(AmalgamationConfig.WEIGHTED_SUM, myConcept, "customFunction");
+
+		// Getting all the attribute descs from the concept to add attributes to the case instance
+		
+
+		IntegerDesc ageDesc = (IntegerDesc) myConcept.getAttributeDesc("age");
+		
+		IntegerDesc offensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("offensive");
+		IntegerDesc defensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("defensive");
+		IntegerDesc fairplayDesc = (IntegerDesc) myConcept.getAttributeDesc("fairplay");
+		IntegerDesc passingDesc = (IntegerDesc) myConcept.getAttributeDesc("passing");
+		IntegerDesc vitalityDesc = (IntegerDesc) myConcept.getAttributeDesc("vitality");
+		IntegerDesc duelsDesc = (IntegerDesc) myConcept.getAttributeDesc("duels");
+		
+		
+		SymbolDesc genderDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("gender");
+		SymbolDesc leagueDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("league");
+		SymbolDesc positionDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("preferred_position");
+		
+		function.setWeight(ageDesc, 1);
+		function.setWeight(genderDesc, 10);
+		function.setWeight(leagueDesc, 5);
+		function.setWeight(positionDesc, 5);
+		
+		function.setWeight(fairplayDesc, 1);
+		function.setWeight(passingDesc, 1);
+		function.setWeight(duelsDesc, 1);
+		function.setWeight(vitalityDesc, 1);
+		
+		int offensiveWeight = 1;
+		int defensiveWeight = 1;
+		
+		switch (position) {
+			case "Torwart":
+				offensiveWeight = 0;
+				defensiveWeight = 5;
+				break;
+			case "Verteidigung":
+				defensiveWeight = 5;
+				break;
+			case "Mittelfeld":
+				offensiveWeight = 3;
+				defensiveWeight = 3;
+				break;
+			case "Sturm":
+				offensiveWeight = 5;
+				break;
+		}
+		function.setWeight(offensiveDesc, offensiveWeight);
+		function.setWeight(defensiveDesc, defensiveWeight);
+
+		
+		return function;
 	}
 
 }
