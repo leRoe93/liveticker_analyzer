3f751034d677bf7d0f96bc395619c4f9051a6957
Leon Römer <roemerl@uni-hildesheim.de>
2020-03-14 18:39:30 +0100

    [IMPL] Check if player already exists in case base.

--- ProjectWork/src/servlets/CreateCaseServlet.java
+++ ProjectWork/src/servlets/CreateCaseServlet.java
@@ -66,52 +66,78 @@ public class CreateCaseServlet extends HttpServlet {
 
 			ICaseBase cb = myproject.getCB("player_cb");
 			
-			String instanceId = Integer.toString(myproject.getAllInstances().size());
+			String existingInstance = playerAlreadyExists(myConcept, cb, request);
 			
-			Instance instance = myConcept.addInstance("player_" + instanceId);
-			
-			StringDesc playerIdDesc = (StringDesc) myConcept.getAttributeDesc("player_id");
-			StringDesc firstNameDesc = (StringDesc) myConcept.getAttributeDesc("first_name");
-			StringDesc lastNameDesc = (StringDesc) myConcept.getAttributeDesc("last_name");
-			StringDesc clubDesc = (StringDesc) myConcept.getAttributeDesc("current_club");
+			if (existingInstance.isEmpty()) {
+				
+				String instanceId = Integer.toString(myproject.getAllInstances().size());
+				
+				Instance instance = myConcept.addInstance("player_" + instanceId);
+				existingInstance = instance.getName();
+				
+				// Getting all the attribute descs from the concept to add attributes to the case instance
+				StringDesc playerIdDesc = (StringDesc) myConcept.getAttributeDesc("player_id");
+				StringDesc firstNameDesc = (StringDesc) myConcept.getAttributeDesc("first_name");
+				StringDesc lastNameDesc = (StringDesc) myConcept.getAttributeDesc("last_name");
+				StringDesc clubDesc = (StringDesc) myConcept.getAttributeDesc("current_club");
+				StringDesc birthdayDesc = (StringDesc) myConcept.getAttributeDesc("birthday");
 
-			
-			IntegerDesc ageDesc = (IntegerDesc) myConcept.getAttributeDesc("age");
-			
-			IntegerDesc offensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("offensive");
-			IntegerDesc defensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("defensive");
-			IntegerDesc fairplayDesc = (IntegerDesc) myConcept.getAttributeDesc("fairplay");
-			IntegerDesc passingDesc = (IntegerDesc) myConcept.getAttributeDesc("passing");
-			IntegerDesc vitalityDesc = (IntegerDesc) myConcept.getAttributeDesc("vitality");
-			IntegerDesc duelsDesc = (IntegerDesc) myConcept.getAttributeDesc("duels");
+				IntegerDesc ageDesc = (IntegerDesc) myConcept.getAttributeDesc("age");
+				IntegerDesc tickerCounterDesc = (IntegerDesc) myConcept.getAttributeDesc("ticker_counter");
+				
+				IntegerDesc offensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("offensive");
+				IntegerDesc defensiveDesc = (IntegerDesc) myConcept.getAttributeDesc("defensive");
+				IntegerDesc fairplayDesc = (IntegerDesc) myConcept.getAttributeDesc("fairplay");
+				IntegerDesc passingDesc = (IntegerDesc) myConcept.getAttributeDesc("passing");
+				IntegerDesc vitalityDesc = (IntegerDesc) myConcept.getAttributeDesc("vitality");
+				IntegerDesc duelsDesc = (IntegerDesc) myConcept.getAttributeDesc("duels");
+				
+				
+				SymbolDesc genderDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("gender");
+				SymbolDesc leagueDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("league");
+				SymbolDesc positionDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("preferred_position");
+				
+				// Filling the instance with information
+				instance.addAttribute(playerIdDesc, instanceId);
+				instance.addAttribute(firstNameDesc, request.getParameter("first_name").toString());
+				instance.addAttribute(lastNameDesc, request.getParameter("last_name").toString());
+				instance.addAttribute(clubDesc, request.getParameter("current_club").toString());
+				instance.addAttribute(birthdayDesc, request.getParameter("birthday").toString());
 
+				instance.addAttribute(offensiveDesc, 5);
+				instance.addAttribute(defensiveDesc, 5);
+				instance.addAttribute(fairplayDesc, 5);
+				instance.addAttribute(passingDesc, 5);
+				instance.addAttribute(vitalityDesc, 5);
+				instance.addAttribute(duelsDesc, 5);
+				
+				instance.addAttribute(ageDesc, calculateAge(request.getParameter("birthday").toString()));
+				instance.addAttribute(tickerCounterDesc, 0);
 
-			SymbolDesc genderDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("gender");
-			SymbolDesc leagueDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("league");
-			SymbolDesc positionDesc = (SymbolDesc) myConcept.getAllAttributeDescs().get("preferred_position");
+				
+				instance.addAttribute(genderDesc, genderDesc.getAttribute(request.getParameter("gender").toString()));
+				instance.addAttribute(leagueDesc, leagueDesc.getAttribute(request.getParameter("league").toString()));
+				instance.addAttribute(positionDesc, positionDesc.getAttribute(request.getParameter("preferred_position").toString()));
+				
+				
+				cb.addCase(instance);
+				myproject.save();
+				
+				String answerWithProfileLink = "<form action=ProfileServlet method=post>"
+						+ "<input type='hidden' name='instance' value='" + existingInstance + "'/>"
+						+ "<button class='btn btn-success btn-link'>Spieler</button>"
+						+ "</form>";
+				
+				request.setAttribute("success", answerWithProfileLink + " <span>wurde erfolgreich der Fallbasis hinzugefuegt!</span>");
+			} else {
+				
+				String answerWithProfileLink = "<form action=ProfileServlet method=post>"
+						+ "<input type='hidden' name='instance' value='" + existingInstance + "'/>"
+						+ "<button class='btn btn-success btn-link'>Spieler</button>"
+						+ "</form>";
+				request.setAttribute("success", answerWithProfileLink + " <span>existiert bereits!</span>");
+			}
 			
-			instance.addAttribute(playerIdDesc, instanceId);
-			instance.addAttribute(firstNameDesc, request.getParameter("first_name").toString());
-			instance.addAttribute(lastNameDesc, request.getParameter("last_name").toString());
-			instance.addAttribute(clubDesc, request.getParameter("current_club").toString());
-			
-			instance.addAttribute(offensiveDesc, 5);
-			instance.addAttribute(defensiveDesc, 5);
-			instance.addAttribute(fairplayDesc, 5);
-			instance.addAttribute(passingDesc, 5);
-			instance.addAttribute(vitalityDesc, 5);
-			instance.addAttribute(duelsDesc, 5);
-			
-			instance.addAttribute(ageDesc, calculateAge(request.getParameter("birthday").toString()));
-			
-			instance.addAttribute(genderDesc, genderDesc.getAttribute(request.getParameter("gender").toString()));
-			instance.addAttribute(leagueDesc, leagueDesc.getAttribute(request.getParameter("league").toString()));
-			instance.addAttribute(positionDesc, positionDesc.getAttribute(request.getParameter("preferred_position").toString()));
-
-			
-			cb.addCase(instance);
-			myproject.save();
-			request.setAttribute("success", "Spieler wurde erfolgreich der Datenbasis hinzugefügt!");
 			request.getRequestDispatcher("addPlayer.jsp").forward(request, response);
 
 
@@ -120,6 +146,44 @@ public class CreateCaseServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	// String used to have the instance name for button link, empty string equals a logical false
+	private String playerAlreadyExists(Concept concept, ICaseBase cb, HttpServletRequest request) {
+		String instanceName = "";
+		
+		// Perform check according to some reliable attribute parameters
+		String firstName = request.getParameter("first_name").toString();
+		String lastName = request.getParameter("last_name").toString();
+		String currentClub = request.getParameter("current_club").toString();
+		String birthday = request.getParameter("birthday").toString();
+		String league = request.getParameter("league").toString();
+		
+		for (Instance instance : cb.getCases()) {
+			
+			String instanceFirstName = instance.getAttForDesc(concept.getAttributeDesc("first_name")).getValueAsString();
+			String instanceLastName = instance.getAttForDesc(concept.getAttributeDesc("last_name")).getValueAsString();
+			String instanceCurrentClub = instance.getAttForDesc(concept.getAttributeDesc("current_club")).getValueAsString();
+			String instanceBirthday = instance.getAttForDesc(concept.getAttributeDesc("birthday")).getValueAsString();
+			String instanceLeague = instance.getAttForDesc(concept.getAttributeDesc("league")).getValueAsString();
+
+			System.out.println("First name: " + instanceFirstName + " equals " + firstName + "?");
+			System.out.println("Last name: " + instanceLastName + " equals " + lastName + "?");
+			System.out.println("birthday: " + instanceBirthday + " equals " + birthday + "?");
+			System.out.println("current club: " + instanceCurrentClub + " equals " + currentClub + "?");
+			System.out.println("league: " + instanceLeague + " equals " + league + "?");
+
+			// Current solution is to have a check for every informational attribute of the player for its entity
+			if (instanceFirstName.equals(firstName) && instanceLastName.equals(lastName) 
+					&& instanceCurrentClub.equals(currentClub) && instanceBirthday.equals(birthday)
+					&& instanceLeague.equals(league)) {
+				
+				System.out.println("Yes, everything matches!");
+				return instance.getName();
+			}
+		}
+		
+		return instanceName;
 	}
 	
 	private int calculateAge(String birthday) {
